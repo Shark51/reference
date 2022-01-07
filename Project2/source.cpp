22aa409ca056a1e11b22dde543bc2966869abea5
Shark51 <dlfrnjs51@naver.com>
2022-01-07 17:13:56 +0900

    2nd

--- Project2/source.cpp
+++ Project2/source.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 
+int change_val(int *p) {
+	*p = 3;
+
+	return 0;
+}
+
+
 int main() {
 	using std::cout;
+	using std::endl;
+	/*
+	int number = 5;
+
+	cout << number << endl;
+	change_val(&number);
+	cout << number << endl;
+	*/
+
+	int a = 3;
+	int &another_a = a;
+
+	another_a = 5;
+	cout << "a: " << a << endl;
+	cout << "another_a: " << another_a << endl;
+
 	cout << "show me something like the best ";
+
 }
